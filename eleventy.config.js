85b4b44fc727a626804c46463e83ba597b6f400f
Fox Mulder <foxmulder@MacBook-Air.local>
2026-05-17 14:50:13 +0400

    speed up img by turning off opts

--- eleventy.config.js
+++ eleventy.config.js
@@ -89,7 +89,7 @@ export default async function(eleventyConfig) {
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// Output formats for each image.
-		formats: ["avif", "webp", "auto"],
+		formats: ["auto"],
 		htmlOptions: {
 			imgAttributes: {
 				alt: "", // required
