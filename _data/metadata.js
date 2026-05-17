5143bac0678dd0f357793d27350685713c000fe1
Fox Mulder <foxmulder@MacBook-Air.local>
2026-05-17 14:16:47 +0400

    ver1

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Весло и парус",
+	url: "https://example.com/",
+	language: "ru",
+	description: "Из всех изобретений и усовершенствований, которых достигли ум и усердие человека, видимо, ни одно не является настолько универсально полезным, плодотворным и необходимым, как искусство мореплавания.",
+	author: {
+		// name: "Your Name Here",
+		// email: "youremailaddress@example.com",
+		// url: "https://example.com/about-me/"
+	}
+}
---
+++ content/about.md
@@ -0,0 +1,7 @@
+---js
+const eleventyNavigation = {
+	key: "Об авторе",
+	order: 3
+};
+---
+# Об авторе
